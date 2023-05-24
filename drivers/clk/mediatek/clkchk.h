3eaa7951f251cffed0951754fd530c323b25bf3b
Sushrut1101 <guptasushrut@gmail.com>
2023-05-24 16:45:41 +0000

    drivers: clk: Import Xiaomi Changes
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- drivers/clk/mediatek/clkchk.h
+++ drivers/clk/mediatek/clkchk.h
@@ -42,6 +42,23 @@ struct regname {
 	const char *name;
 };
 
+enum FMETER_TYPE {
+	FT_NULL,
+	ABIST,
+	CKGEN,
+	ABIST_2,
+	SUBSYS,
+};
+
+struct fmeter_clk {
+	enum FMETER_TYPE type;
+	u32 id;
+	const char *name;
+	u32 ofs;
+	u32 pdn;
+	u32 grp;
+};
+
 struct clkchk_op {
 	const char *(*get_vf_name)(int id);
 	int (*get_vf_opp)(int id, int opp);

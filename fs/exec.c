e4aa88c5d7f2a58714e3e55b619731418efd78d3
Sushrut1101 <guptasushrut@gmail.com>
2023-05-24 16:34:19 +0000

    fs: Import Xiaomi Changes
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -967,9 +967,9 @@ int kernel_read_file_from_path(const char *path, void **buf, loff_t *size,
 		return -EINVAL;
 
 	file = filp_open(path, O_RDONLY, 0);
-	if (IS_ERR(file))
+	if (IS_ERR(file)){
 		return PTR_ERR(file);
-
+	}
 	ret = kernel_read_file(file, buf, size, max_size, id);
 	fput(file);
 	return ret;
